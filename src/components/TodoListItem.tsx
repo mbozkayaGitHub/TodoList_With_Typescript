5d9a2ee5730eac7fa851b689a25797aa9ebb2440
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-12-07 08:46:25 +0300

    Ternary

---
+++ src/components/TodoListItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface IListItem {
+  item: TodoType;
+ 
+}
+
+
+const TodoListItem:React.FC<IListItem> = ({item}) => {
+  return (
+    <li>
+   {   item.isDone ? <p className='checked'>{item.task} </p>
+      :<p> {item.task} </p>}
+      <span className="task-icons" >✖️</span>
+    </li>
+  );
+};
+
+export default TodoListItem
