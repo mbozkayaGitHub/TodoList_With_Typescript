e3581795fd84e97654ef43f8e14ef9aa6fa9ad56
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-12-09 23:50:44 +0300

    toggleClear

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,7 +5,7 @@ const TodoList: React.FC<ITodoList> = ({ todos , toggleTodo}) => {
   return (
     <ul>
       {todos.map((item) => (
-        <TodoListItem key={item.id} item={item} toggle={} />
+        <TodoListItem key={item.id} item={item}  />
       ))}
     </ul>
   );
