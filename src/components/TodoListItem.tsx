a17da3a8a2909d329e79d0c9edcd534f37925d80
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-12-12 14:19:07 +0300

    TaskDone

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 interface IListItem {
   item: TodoType;
+  toggleTodo:ToggleFn
  
 }
 
 
-const TodoListItem:React.FC<IListItem> = ({item}) => {
+const TodoListItem:React.FC<IListItem> = ({item,toggleTodo}) => {
   return (
-    <li>
-   {   item.isDone ? <p className='checked'>{item.task} </p>
-      :<p> {item.task} </p>}
+    <li >
+   {   item.isDone ? <p className='checked' onClick={()=> toggleTodo(item)}>{item.task} </p>
+      :<p onClick={()=> toggleTodo(item)}> {item.task} </p>}
       <span className="task-icons" >✖️</span>
     </li>
   );
